2c32a68fffbd7aefef7c9ef91099ff25be2d5fb1
Ankit Kumar <ankitmandal1079@gmail.com>
2025-12-28 14:26:21 +0530

    impleted the session model / controllers / routes

--- backend/src/config/stream.js
+++ backend/src/config/stream.js
@@ -1,4 +1,5 @@
 import {StreamChat} from "stream-chat"
+import {StreamClient} from "@stream-io/node-sdk"
 import {ENV} from "./env.js"
 
 const apiKey = ENV.STREAM_API_KEY
@@ -7,8 +8,8 @@ const apiSecret = ENV.STREAM_API_SECRET
 if(!apiKey || !apiSecret){
     console.error("STREAM_API_KEY or STREAM_API_SECRET is missing !")
 }
-
-export const chatClient = StreamChat.getInstance(apiKey,apiSecret)
+export const streamClient = new StreamClient(apiKey,apiSecret) // for video sessions
+export const chatClient = StreamChat.getInstance(apiKey,apiSecret) // for chat purpose
 
 export const upsertStreamUser = async(userData) => {
     try{
